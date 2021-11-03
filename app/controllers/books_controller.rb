6b858086abe1012c74bb96b161524132fb5bf822
EC2 Default User <ec2-user@ip-172-31-35-151.ec2.internal>
2021-11-03 23:31:22 +0000

    [Finish]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,16 +8,14 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  # def new
-  #   @book = Book.new
-  # end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-      redirect_to books_path, notice: 'Book was successfully created.'
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
     else
-      redirect_to books_path, notice: 'メッセージの送信に失敗しました'
+      @books = Book.all
+      render:index
     end
   end
 
@@ -27,18 +25,19 @@ class BooksController < ApplicationController
 
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-      redirect_to book_path(book.id) , notice:'Book was successfully updated.'
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id) , notice:'Book was successfully updated.'
     else
-      redirect_to edit_book_path, notice:'失敗.'
+      render:edit
+      @book = Book.find(params[:id])
     end
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice:'Book was successfully destroyed.'
   end
 
   private
